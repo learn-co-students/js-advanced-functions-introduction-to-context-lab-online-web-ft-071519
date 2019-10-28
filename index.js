d3a50ad26a191b45bac0d92ec9d3e0bd4b7d13fa
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-28 19:38:01 +0000

    Done.

--- index.js
+++ index.js
@@ -29,3 +29,51 @@ function createTimeInEvent(employee,datestamp){
         date,})
 return employee
 }
+
+function createTimeOutEvent(employee,datestamp){
+  let [date, hour] = datestamp.split(' ')
+  employee.timeOutEvents.push({       
+       type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,})
+return employee  
+}
+
+function hoursWorkedOnDate(employee, dateday){
+  let inobject = employee.timeInEvents.find(function(e){
+        return e.date === dateday
+    })
+    
+   let   outobject = employee.timeOutEvents.find(function(e){
+        return e.date === dateday
+    })
+    
+    return (outobject.hour - inobject.hour)/100
+}
+
+function wagesEarnedOnDate(employee, dateday){
+  return hoursWorkedOnDate(employee, dateday) * employee.payPerHour
+  
+}
+
+function allWagesFor(employee){
+  let sum = 0
+  employee.timeInEvents.forEach(function(e) {
+   sum = sum+ wagesEarnedOnDate(employee, e.date)
+  });
+  return sum
+}
+function calculatePayroll(arrayemployees){
+  let sum = 0
+  arrayemployees.forEach(function(employee){
+   sum = sum+ allWagesFor(employee)
+  })
+  return sum
+}
+
+function findEmployeeByFirstName(arrayemployees,firstName){
+  let result = arrayemployees.find(function(employee){
+        return employee.firstName === firstName
+    })
+    return result
+}
